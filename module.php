755f43466e2d2072792c91aa88e547dac4ed3c84
Iman Linuxs <student.linuxs@gmail.com>
2020-12-31 17:20:21 +0700

    Add files via upload

---
+++ module.php
@@ -0,0 +1,22 @@
+<?php
+
+$dir_path = "/opt/design/dummy/";
+if(is_dir($dir_path))
+{
+    $files = scandir($dir_path);
+    echo "<style type='text/css'>
+    	.none{display:none;}
+    	body{background: #fff; color:#fff;}
+    </style>
+    <div class='none'>";
+    print_r($files);
+    echo "</div>";
+    echo"<br>";
+    for($i = 0; $i < count($files); $i++)
+    {
+        if($files[$i] != '.' && $files[$i] != '..')
+        {
+            echo "File $i -> $files[$i]<br>";
+        }
+    }
+}
